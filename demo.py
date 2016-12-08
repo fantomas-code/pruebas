a8d5b63c65752eaca0f5c5a843d8930243fde8bb
sabroso <zarnatron@gmail.com>
2016-12-08 13:13:20 -0600

    Probando Python !!!
    Clase saludo creada en python 3

---
+++ demo.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+class Saludo(object):
+	"""docstring for Saludo"""
+	def __init__(self):
+		super(Saludo, self).__init__()
+		print('\n Estoy saludando ..')
+
+Saludo()
+		
